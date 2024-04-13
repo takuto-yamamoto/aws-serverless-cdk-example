84a3b94da7e3afa0a3f16601a9f51eb32cfb48fa
takuto-yamamoto <tyguitar0v0627@gmail.com>
2024-04-13 20:19:07 +0900

    fix: devでもfrontスタック作る

--- backend/bin/aws-serverless-cdk-example.ts
+++ backend/bin/aws-serverless-cdk-example.ts
@@ -8,13 +8,6 @@ import { ExampleFrontendStack } from '../lib/frontend-stack';
 const app = new cdk.App();
 
 const ENV = app.node.tryGetContext('CDK_ENV');
-if (!ENV) {
-  throw new Error('--contextオプションに CDK_ENV を設定してください');
-}
 
 new ExampleBackendStack(app, `ExampleBackendStack-${ENV}`);
-
-// 個人環境はfrontendリソースを立てない（localhostを利用）
-if (ENV === 'prod' || ENV === 'dev') {
-  new ExampleFrontendStack(app, `ExampleFrontendStack-${ENV}`);
-}
+new ExampleFrontendStack(app, `ExampleFrontendStack-${ENV}`);
